bc2a4d4c2c269cce1e790f19bae08faa92e5cf7e
Prath7057 <iamprathpatil@gmail.com>
2025-01-26 01:47:24 +0530

    `Refactor SecondApp component to conditionally render buttons based on counter value`

--- 02-counter/src/SecondApp.jsx
+++ 02-counter/src/SecondApp.jsx
@@ -1,19 +1,20 @@
 import React from 'react';
 
 const SecondApp = ({ counter, onAdd, onRemove }) => {
-  return (
-    <div className="container mt-5">
-      <h1 className="text-center">Counter: {counter}</h1>
-      <div className="d-flex justify-content-center gap-3 mt-3">
-        <button className="btn btn-primary" onClick={onAdd}>
-          Add
-        </button>
-        <button className="btn btn-danger" onClick={onRemove}>
-          Remove
-        </button>
-      </div>
-    </div>
-  );
+    return (
+        <div className="container mt-5">
+            <h1 className="text-center">Counter: <span style={{display:`inline-block`,width:`2rem`}}>{counter}</span></h1>
+            <div className="d-flex justify-content-center gap-3 mt-3">
+                <button style={{ visibility: counter <= 9 ? 'visible' : 'hidden' }} className="btn btn-primary" onClick={onAdd}>
+                    Add
+                </button>
+
+                <button style={{ visibility: counter > 1 ? 'visible' : 'hidden' }} className="btn btn-danger" onClick={onRemove}>
+                    Remove
+                </button>
+            </div>
+        </div>
+    );
 };
 
 export default SecondApp;
