ba4a4c02ee87449571aacb43789ee64bc9f827cf
Prath7057 <iamprathpatil@gmail.com>
2025-02-02 01:05:17 +0530

    feat: set up initial React app with Vite and Tailwind CSS

---
+++ password-generator/src/App.jsx
@@ -0,0 +1,42 @@
+import { useCallback, useState } from 'react'
+
+function App() {
+  const [length, setLength] = useState(8);
+  const [numberAllowed, setNumberAllowed] = useState(false);
+  const [charAllowed, setCharAllowed] = useState(false);
+  const [password, setPassword] = useState('');
+
+  const passwordGenerator = useCallback(() => {
+    let pass = ""
+    let str = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz";
+
+    if (numberAllowed) {
+      str += "0123456789";
+    }
+    if (charAllowed) {
+      str += "!@#$%^&*()_+";
+    }
+    for (let i = 0; i < length; i++) {
+      let char = Math.floor(Math.random() * str.length);
+      pass += str.charAt(char);
+    }
+    setPassword(pass);
+  }, [length, numberAllowed, charAllowed, setPassword]);
+
+  return (
+    <>
+      <div className='w-full max-w-md mx-auto shadow-md rounded-lg 
+      px-4 py-3 my-8 text-orange-500 bg-gray-800' >
+      <h1 className='text-white text-center my'>Password Generator</h1>
+        <div className='flex shadow rounded-lg overflow-hidden mb-4'>
+          <input type="text" value={password}
+            className='outline-none w-full py-1 px-3'
+            placeholder='Password' readOnly />
+        </div>
+
+      </div>
+    </>
+  )
+}
+
+export default App
