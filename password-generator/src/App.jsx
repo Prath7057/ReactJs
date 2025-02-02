30d6c71b563db0304de4fe2412de9617c52e228f
Prath7057 <iamprathpatil@gmail.com>
2025-02-03 00:13:54 +0530

    feat: implement password generator UI with controls for length and character options

--- password-generator/src/App.jsx
+++ password-generator/src/App.jsx
@@ -23,15 +23,50 @@ function App() {
     setPassword(pass);
   }, [length, numberAllowed, charAllowed, setPassword]);
 
+  passwordGenerator();
+
   return (
     <>
       <div className='w-full max-w-md mx-auto shadow-md rounded-lg 
-      px-4 py-3 my-8 text-orange-500 bg-gray-800' >
-      <h1 className='text-white text-center my'>Password Generator</h1>
+      px-4 py-3 my-8 text-orange-500 bg-gray-800'>
+        <h1 className='text-white text-center my'>Password Generator</h1>
         <div className='flex shadow rounded-lg overflow-hidden mb-4'>
           <input type="text" value={password}
             className='outline-none w-full py-1 px-3'
             placeholder='Password' readOnly />
+          <button className='outline-none bg-blue-700 text-white px-2 py-0.5 shrink-0'>Copy</button>
+        </div>
+        <div className='flex text-sm gap-x-2'>
+          <div className='flex items-center gap-x-1'>
+            <input type='range'
+              min={6}
+              max={100}
+              value={length}
+              className='cursor-pointer'
+              onChange={(e) => setLength(e.target.value)}
+            />
+            <label className='text-white'>{length}</label>
+          </div>
+          <div className='flex items-center gap-x-1'>
+            <input type='checkbox'
+              defaultChecked={numberAllowed}
+              id='numberInput'
+              onChange={() => {
+                setNumberAllowed((prev) => !prev);
+              }}
+            />
+            <label htmlFor='numberInput'>Numbers</label>
+          </div>
+          <div className='flex items-center gap-x-1'>
+            <input type='checkbox'
+              defaultChecked={charAllowed}
+              id='characterInput'
+              onChange={() => {
+                setCharAllowed((prev) => !prev);
+              }}
+            />
+            <label htmlFor='characterInput'>Characters</label>
+          </div>
         </div>
 
       </div>
