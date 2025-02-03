e3a41dc1910520fa53e3e109239f522ae111c8e6
Prath7057 <iamprathpatil@gmail.com>
2025-02-03 10:20:34 +0530

    feat: add useEffect for password generation and update copy button functionality

--- password-generator/src/App.jsx
+++ password-generator/src/App.jsx
@@ -1,4 +1,4 @@
-import { useCallback, useState } from 'react'
+import { useCallback, useEffect, useState } from 'react'
 
 function App() {
   const [length, setLength] = useState(8);
@@ -23,18 +23,22 @@ function App() {
     setPassword(pass);
   }, [length, numberAllowed, charAllowed, setPassword]);
 
-  passwordGenerator();
+  useEffect(() => {
+    passwordGenerator();
+  }, [length,numberAllowed,charAllowed,passwordGenerator]);
 
   return (
     <>
       <div className='w-full max-w-md mx-auto shadow-md rounded-lg 
       px-4 py-3 my-8 text-orange-500 bg-gray-800'>
-        <h1 className='text-white text-center my'>Password Generator</h1>
+        <h1 className='text-white text-center my-3'>Password Generator</h1>
         <div className='flex shadow rounded-lg overflow-hidden mb-4'>
           <input type="text" value={password}
             className='outline-none w-full py-1 px-3'
             placeholder='Password' readOnly />
-          <button className='outline-none bg-blue-700 text-white px-2 py-0.5 shrink-0'>Copy</button>
+          <button className='outline-none bg-blue-700 text-white px-2 py-0.5 shrink-0'
+            onClick={() => {  navigator.clipboard.writeText(password) }}
+          >Copy</button>
         </div>
         <div className='flex text-sm gap-x-2'>
           <div className='flex items-center gap-x-1'>
